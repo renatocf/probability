7bfff2e270d6f43d79c2f826125ea5fd26b8ffd7
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2016-05-16 20:32:29 -0300

    Add flags and info in Config.mk

--- Config.mk
+++ Config.mk
@@ -1,61 +1,27 @@
 
-############################################################
-##     UNCOMMENT ANY TARGET TO OVERWRITE THE DEFAULT!     ##
-############################################################
+################################################################################
+##                                PROBABILITY                                 ##
+################################################################################
 
 # Project info
 # ===============
-# PROJECT         := # Project name (def: Default)
-# VERSION         := # Version (def: 1.0.0)
-# STD_NAMESPACE   := # Project namespace for C/C++
-# GIT_REMOTE_PATH := # Remote path for git repository
-# MAINTEINER_NAME := # Your name
-# MAINTEINER_MAIL := # your_name@mail.com
-# COPYRIGHT       := # Copyright Holder
-# SYNOPSIS        := # One-line description of the program
-# DESCRIPTION     := # Longer description of the program
-
-# Program settings
-# ==================
-# BIN             := # Binaries' names. If a subdir of any
-                     # src dir has the same name of this bin
-                     # it and all its subdir will be compiled
-                     # only for this specific binary
-# ARLIB           := # Static/Shared libraries' names. If
-# SHRLIB          := # one is a dir, all srcs within will
-                     # make the lib
-
-# Dependencies
-# ==============
-# GIT_DEPENDENCY  := # List of git dependencies in the form: 
-#                    # DEP_NAME => dep_path build_cmd        
-# WEB_DEPENDENCY  := # Same as above, but for URL downloads  
-#                    # with 'curl -o' (default) or 'wget -O' 
-
-# Paths
-# =======
-# ASLIBS          := # Assembly paths
-# CLIBS           := # C paths
-# CXXLIBS         := # C++ paths
-# LDLIBS          := # Linker paths
+PROJECT         := Probability
+VERSION         := 0.0.1
+STD_NAMESPACE   := probability
+GIT_REMOTE_PATH := # Remote path for git repository
+MAINTEINER_NAME := Renato Cordeiro Ferreira
+MAINTEINER_MAIL := renato.cferreira@hotmail.com
+COPYRIGHT       := Renato Cordeiro Ferreira
+SYNOPSIS        := A fast implementation of probabilities
+DESCRIPTION     := A fast implementation of probabilities, \
+                   using log space
 
 # Flags
 # =======
-# CPPFLAGS        := # Precompiler Flags
-# ASFLAGS         := # Assembly Flags
-# CFLAGS          := # C Flags
-# CXXFLAGS        := # C++ Flags
-# LDFLAGS         := # Linker flags
-
-# Documentation
-# ===============
-# LICENSE         := # File with a License (def: LICENSE)
-# NOTICE          := # Notice of the License, to be put in 
-#                    # the top of any file (def: NOTICE).
-# DOXYFILE        := # Dxygen config file (def: Doxyfile)
+CPPFLAGS        := # Precompiler Flags
+CXXFLAGS        := -std=c++14 -Wall -Wextra -Wpedantic -Wshadow -O3
+LDFLAGS         := # Linker flags
 
 # Makeball list
 # ===============
-# 'include conf/makeball.mk' for pre-configured options
-# to use the library 'makeball'
-
+include conf/googletest.mk
